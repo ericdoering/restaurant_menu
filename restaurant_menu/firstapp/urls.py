0a0e8947372702aec4c78fb469aa6ed6fdd3d808
Eric Doering <ericbenjamindoering@gmail.com>
2025-07-03 16:36:33 -0400

    template added and new reservation form built

--- restaurant_menu/firstapp/urls.py
+++ restaurant_menu/firstapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('function', views.hello_world),
     path('class', views.HelloNewYork.as_view()),
+    path('reservation', views.home),
 ]
--- restaurant_menu/firstapp/views.py
+++ restaurant_menu/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
@@ -10,3 +11,14 @@ class HelloNewYork(View):
     def get(self, request):
         return HttpResponse('Hello New York!')
 
+
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('SUCCESS')
+        
+    return render(request, 'index.html', { 'form': form })
